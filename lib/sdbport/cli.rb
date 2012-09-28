e089759c6b595ec8071dbe389405e57ab6e04e8d
Brett Weaver <brett@weav.net>
2012-09-28 13:35:11 -0700

    refactoring

--- lib/sdbport/cli.rb
+++ lib/sdbport/cli.rb
@@ -1,15 +1,46 @@
 require 'trollop'
 
 module Sdbport
-  module CLI
-    def self.start
+  class CLI
+
+    def start
       cmd = ARGV.shift
+
+      @opts = read_options
+
+      options = { :access_key => @opts[:access_key],
+                  :secert_key => @opts[:secret_key],
+                  :region     => @opts[:region] }
 
       case cmd
       when 'export'
-        puts 'export'
+        @sdbport = AWS::SimpleDB.new options
+        puts @sdbport.export
       when 'import'
-        puts 'import'
+        @sdbport = AWS::SimpleDB.new options
+      end
+    end
+
+    private
+
+    def read_options
+      Trollop::options do
+        version Sdbport::VERSION
+        banner <<-EOS
+
+Export simpledb domain.
+
+Usage:
+
+sdbport export --access-key=xxx --secret-key=yyy -r us-west-1
+
+EOS
+        opt :help, "Display Help"
+        opt :region, "AWS region", :type => :string
+        opt :access_key, "AWS Access Key ID", :type => :string,
+                                              :short => :none
+        opt :secret_key, "AWS Secret Access Key", :type => :string,
+                                                  :short => :none
       end
     end
   end
